76f5f7353644fa189575fa50808aa8b93df54047
rizwan <rizwan.shaikh@cuelogic.com>
2018-08-26 01:11:14 +0530

    format code

--- server.js
+++ server.js
@@ -4,45 +4,46 @@ const bodyparser = require('body-parser');
 
 const app = new express();
 
-var dict = [
-    {
-        term:"Rock",
-        defined:"Solid object to throw on someone"
+var dict = [{
+        term: "Rock",
+        defined: "Solid object to throw on someone"
     },
     {
-        term:"Milk",
-        defined:"Liquid"
+        term: "Milk",
+        defined: "Liquid"
     },
     {
-        term:"Persue",
-        defined:"Follw someone"
+        term: "Persue",
+        defined: "Follw someone"
     }
 ];
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log(`${req.method} for ${req.url}`);
     next();
 });
 app.use(express.static('./public'));
 app.use(cors());
 
-app.get('/dictionary-api',(req,res)=>{
+app.get('/dictionary-api', (req, res) => {
     res.json(dict);
 });
 
-app.post('/dictionary-api',(req,res)=>{
+app.post('/dictionary-api', (req, res) => {
     var item = {
-        term:req.body.term,
-        defined : req.body.defined
+        term: req.body.term,
+        defined: req.body.defined
     }
     dict.push(item);
     res.json(dict);
 });
-app.delete('/dictionary-api/:term',(req,res)=>{
+app.delete('/dictionary-api/:term', (req, res) => {
     let index = dict.indexOf(req.params.term);
-    dict = dict.splice(index,1);
+    dict = dict.splice(index, 1);
     res.json(dict);
 });
 
